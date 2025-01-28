2abd991c484a4f571dd46fbb875bcb33233a9d91
sergi1307 <sergi13072006@gmail.com>
2025-01-28 12:28:54 +0100

    Hacemos estructura principal del Main

--- Main.java
+++ Main.java
@@ -2,14 +2,43 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int opc;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("1. Imprimir tabla de multiplicar.");
+            System.out.println("2. Multiplicar dos números.");
+            System.out.println("3. Comprobar Pitágoras.");
+            System.out.println("4. Calcular porcentaje descuento.");
+            System.out.println("5. Decir positivo negativo o cero.");
+            System.out.println("6. Pasar de millas a km.");
+            System.out.println("7. Salir.");
+
+            opc = Leer.leerEntero("Introduzca que quiere hacer: ");
+
+            switch (opc) {
+                case 1:
+                    System.out.println("Opción 1.");
+                    break;
+                case 2:
+                    System.out.println("Opción 2.");
+                    break;
+                case 3:
+                    System.out.println("Opción 3.");
+                    break;
+                case 4:
+                    System.out.println("Opción 4.");
+                    break;
+                case 5:
+                    System.out.println("Opción 5.");
+                    break;
+                case 6:
+                    System.out.println("Opción 6.");
+                case 7:
+                    System.out.println("Nos vemos en la próxima.");
+                default:
+                    System.out.println("Número introducido no válido.");
+            }
+
+        } while (opc != 7);
     }
 }
