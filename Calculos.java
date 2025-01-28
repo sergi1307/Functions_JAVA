e88e80a49276d4ded3fb215781de23d01bd2316d
sergi1307 <sergi13072006@gmail.com>
2025-01-28 13:16:17 +0100

    Creamos la estructura para las 3 funciones que vamos a tener en la clase Calculos

--- Calculos.java
+++ Calculos.java
@@ -1,2 +1,13 @@
 public class Calculos {
+    public static double queDescuento(int cant, int cant_desc) {
+        return 0;
+    }
+
+    public static int dimeSigno(int num1) {
+        return 0;
+    }
+
+    public static double millas_a_km(int num1) {
+        return 0;
+    }
 }
