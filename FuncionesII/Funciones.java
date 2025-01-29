2da70decf55b51b29597786d0740dc47b082bc21
sergi1307 <sergi13072006@gmail.com>
2025-01-29 10:29:54 +0100

    Preparamos las Funciones II con la estructura principal

---
+++ FuncionesII/Funciones.java
@@ -0,0 +1,46 @@
+package FuncionesII;
+
+public class Funciones {
+
+    public static void mostrarVectorInt(int[] vector) {
+
+    }
+
+    public static void mostrarMatrizInt(int[][] matriz) {
+
+    }
+
+    public static void copiar2Vectores(int[] vector1, int[] vector2) {
+
+    }
+
+    public static void copiarVector(int[] vector) {
+
+    }
+
+    public static boolean igualesVectores(int[] vector1, int[] vector2) {
+        return false;
+    }
+
+    public static int[] vectorAleatorio(int tamanyo) {
+        int[] vector = new int[tamanyo];
+        return vector;
+    }
+
+    public static int mediaVector(int[] vector) {
+        return 0;
+    }
+
+    public static boolean esMayorEdad(int edad) {
+        return false;
+    }
+
+    public static int fibonacci(int num) {
+        return 0;
+    }
+
+    public static void aproximaPhi(int num) {
+
+    }
+
+}
---
+++ FuncionesII/Main.java
@@ -0,0 +1,48 @@
+package FuncionesII;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int opc;
+
+        do {
+            System.out.println("1 - MostrarVectorInt");
+            System.out.println("2 - MostrarMatrizInt");
+            System.out.println("3 - Copiar2Vectores");
+            System.out.println("4 - CopiarVector");
+            System.out.println("5 - IgualesVectores");
+            System.out.println("6 - vectorAleatorio");
+            System.out.println("7 - MediaVector");
+            System.out.println("8 - EsMayorDeEdad");
+            System.out.println("9 - Fibonacci");
+            System.out.println("10 - AproximaPhi");
+
+            opc = Leer.leerEntero("Introduzca que quiere hacer: ");
+
+            switch (opc) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+            }
+        } while (opc != 0);
+    }
+}
